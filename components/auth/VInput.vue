c321d8711495ff99c0bfee14284de3fcac2711ab
Alan <alan.b@realtyna.net>
2022-05-21 09:11:15 -0400

    component input for auth

---
+++ components/auth/VInput.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div :class="[$parent.hasError(keyValidation) ? 'invalid-input': '','form-field' ,'d-flex ','align-items-center']">
+      <span class="icon"><fa :icon="icon"/></span>
+      <input @keyup="$parent.validate(keyValidation)" @blur="$parent.validate(keyValidation)"
+             @keydown="$parent.validate(keyValidation)"
+             v-bind="$attrs"
+             v-bind:value="value"
+             v-on:input="$emit('input', $event.target.value)"
+             :type="type"
+             :name="keyValidation"
+             :placeholder="label">
+    </div>
+    <div class="invalid-message" v-if="$parent.hasError(keyValidation)">{{ $parent.errorMessage(keyValidation) }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VInput",
+  props: {
+    value: [String, Number],
+    type: {
+      type: String,
+      default: 'text'
+    },
+    keyValidation: String,
+    icon: {
+      type: Object,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+@import "assets/scss/auth.scss";
+</style>
