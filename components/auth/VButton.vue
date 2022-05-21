29ccd63c2ecf2fa22cf8331ea09b4414c8661f16
Alan <alan.b@realtyna.net>
2022-05-21 10:43:49 -0400

    component button

---
+++ components/auth/VButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="$emit('actionClick')" :disabled="$parent.loading" :type="type" class="btn mt-3 btn-login">
+      <span v-if="$parent.loading"><LoaderDots></LoaderDots></span>
+      <span v-else>{{ label }}</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VButton",
+  props: {
+    type: {
+      type: String,
+      default: 'submit'
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "assets/scss/auth.scss";
+</style>
