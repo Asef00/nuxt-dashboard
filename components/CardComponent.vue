2095afe94fdf04b424b3463aa6239a1b836531ad
Asef <50338626+Asef00@users.noreply.github.com>
2022-05-24 14:08:07 +0430

    vue multiselect form component add other font weights

--- components/CardComponent.vue
+++ components/CardComponent.vue
@@ -10,22 +10,39 @@
         accusantium molestiae eius deleniti. Laborum totam nam incidunt placeat
         voluptatem molestias?
       </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste incidunt
-        quae delectus necessitatibus? Dolorem similique dolore laborum sint a
-        accusantium molestiae eius deleniti. Laborum totam nam incidunt placeat
-        voluptatem molestias?
-      </p>
+      <form action="" class="c-form">
+        <h4 class="c-form__title">Sync Accounts</h4>
+        <MultiselectComponent />
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import MultiselectComponent from "./form/MultiselectComponent.vue";
+
 export default {
+  components: { MultiselectComponent },
   name: "FooterPartial",
 };
 </script>
 
 <style scoped lang="scss">
 @import "~/assets/scss/components/_card.scss";
+</style>
+
+<style lang="scss">
+.c-form__title {
+  font-size: 14px;
+  color: $primary-color;
+  font-weight: 600;
+}
+
+.c-form__label {
+  @include font-size(14px);
+  font-weight: 500;
+  color: $text-color;
+  margin-bottom: 0.5em;
+  display: block;
+}
 </style>
