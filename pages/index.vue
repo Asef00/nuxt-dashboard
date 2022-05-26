d61dc696a386010e7de158b11bd514237385b25c
Alan <alan.b@realtyna.net>
2022-05-26 09:53:00 -0400

    create select component

--- pages/index.vue
+++ pages/index.vue
@@ -6,48 +6,15 @@
     <div class="c-card__body">
       <form action="" class="c-form">
         <h4 class="c-form__title">Sync Accounts</h4>
-
-        <div class="c-form__control">
-          <label class="c-form__label">Single select</label>
-          <multiselect
-            v-model="value"
-            :options="options"
-            :select-label="'Selected'"
-            :deselect-label="'Hit to remove'"
-            placeholder="Select Option"
-          >
-          </multiselect>
-        </div>
-
-        <div class="c-form__control">
-          <label class="c-form__label">Single select</label>
-          <multiselect
-            v-model="value"
-            :options="options"
-            :select-label="'Selected'"
-            :deselect-label="'Hit to remove'"
-            placeholder="Select Option"
-            class="is-invalid"
-          >
-          </multiselect>
-          <span class="c-form__error">Please select a correct option</span>
-        </div>
-
+        <VSelect :multiple="true" placeholder="Please select tag"  v-model="payload.tag" :list="options" key-validation="tag" label="Tags"/>
         <div class="row">
           <div class="col-md-6">
-            <VInput  label="Name" v-model="payload.name" @some-event="validate('name')" keyValidation="name" placeholder="Name enter"/>
+            <VInput  label="Name" v-model="payload.name" keyValidation="name" placeholder="Name enter"/>
           </div>
           <div class="col-md-6">
             <div class="c-form__control">
               <label class="c-form__label">Sample Label</label>
               <input class="c-form__input" type="text" placeholder="Disabled" disabled/>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="c-form__control">
-              <label class="c-form__label">Sample Label</label>
-              <input class="c-form__input is-invalid" @keyup="validate('email')" type="text" placeholder="Enter..." value="jaguar@bla.com"/>
-              <span class="c-form__error">Please provide a valid Email</span>
             </div>
           </div>
           <div class="col-md-6">
@@ -77,30 +44,29 @@
 </template>
 
 <script>
-import Multiselect from "vue-multiselect";
 import * as Yup from "yup";
 export default {
-  components: {Multiselect},
   data() {
     return {
-      value: null,
-      test:'dsdf',
       payload: {
-        name: 'hi'
+        name: '',
+        tag: '',
       },
       options: ["list", "of", "options", "bla"],
     };
   },
   methods: {
-    validation(roles) {
+    validation() {
       return Yup.object({
         name: Yup.string().required(),
+        tag: Yup.array().min(1),
       });
     },
     resetError() {
       this.$store.commit('me/RESET_ERROR')
       this.errors = {
         name: '',
+        tag: '',
       }
     },
   },
