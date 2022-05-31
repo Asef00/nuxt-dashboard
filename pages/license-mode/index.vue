c23c00ab3d4b29eed44940f4a59b97b5ccfb712c
Alan <alan.b@realtyna.net>
2022-05-31 10:33:10 -0400

    create license mode

---
+++ pages/license-mode/index.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "index",
+  created() {
+    this.setTitle('License Mode')
+    this.setBreadcrumb([
+      {
+        to: '/license-mode',
+        name: 'License Mode'
+      }
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
