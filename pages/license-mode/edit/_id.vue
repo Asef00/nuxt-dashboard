08565ff76088b68aa120c2c9ea5b3c0dde50b8c0
Alan <alan.b@realtyna.net>
2022-05-31 14:51:12 -0400

    edit license mode

---
+++ pages/license-mode/edit/_id.vue
@@ -0,0 +1,107 @@
+<template>
+  <VCard title="Create New License Mode">
+    <form @submit.prevent="update" class="c-form">
+      <div class="row">
+        <div class="col-md-6">
+          <VInput
+            @validation="validate('name')"
+            :error="errorMessage('name')"
+            label="Name"
+            v-model="payload.name"
+            placeholder="Please enter name"
+          />
+        </div>
+        <div class="col-md-6">
+          <VInput
+            @validation="validate('label')"
+            :error="errorMessage('label')"
+            label="Label"
+            v-model="payload.label"
+            placeholder="Please enter label"
+          />
+        </div>
+      </div>
+      <VBtn :loader="loaderRequest">SAVE</VBtn>
+    </form>
+  </VCard>
+</template>
+
+<script>
+import * as Yup from "yup";
+
+export default {
+  name: "edit",
+  data() {
+    return {
+      payload: {
+        name: '',
+        label: '',
+      }
+    };
+  },
+  methods: {
+    update() {
+      this.startLoading();
+      this.validation()
+        .validate(this.payload, {abortEarly: false})
+        .then(async () => {
+          this.resetError();
+          await this.$store.dispatch("licenseMode/update", {
+            payload:this.payload,
+            id:this.$route.params.id
+          });
+          this.stopLoading();
+          const err = this.handleError(this.$store.state.licenseMode.error);
+          if (!err) {
+            this.$toast.success("License Mode successfully updated.");
+            this.$router.push("/license-mode");
+          }
+        })
+        .catch((err) => {
+          this.setAllErrorValidation(err);
+          this.stopLoading();
+        });
+    },
+    async show() {
+      this.startLoading()
+      await this.$store.dispatch("licenseMode/show", this.$route.params.id);
+      this.handleError(this.$store.state.licenseMode.error);
+      let data = this.$store.state.licenseMode.item;
+      this.payload.label = data.label
+      this.payload.name = data.name
+      this.stopLoading()
+    },
+    validation() {
+      return Yup.object({
+        name: Yup.string().required(),
+        label: Yup.string().required(),
+      });
+    },
+    resetError() {
+      this.errors = {
+        name: "",
+        label: "",
+      };
+    }
+  },
+  created() {
+    this.show()
+    this.setTitle('License Mode')
+    this.setBreadcrumb([
+      {
+        to: '/license-mode',
+        name: 'License Mode'
+      },
+      {
+        to: '/license-mode/create',
+        name: 'Create'
+      }
+    ])
+    this.resetError()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
