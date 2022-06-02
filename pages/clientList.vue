d38ce72fbda5b1dcc32a63c30d049abbc44388ad
Asef <50338626+Asef00@users.noreply.github.com>
2022-06-02 10:13:10 -0400

    remove sort feature

--- pages/clientList.vue
+++ pages/clientList.vue
@@ -16,7 +16,6 @@
               <!-- NOTE: non column props should start with _ -->
               <th
                 v-if="!col.startsWith('_')"
-                v-on:click="sortTable(col)"
                 v-bind:class="[
                   col == sortColumn ? 'is-active' : '',
                   col == 'note' ? 'u-text-center' : '',
@@ -201,27 +200,7 @@ export default {
       ],
     };
   },
-  methods: {
-    sortTable: function sortTable(col) {
-      if (this.sortColumn === col) {
-        this.ascending = !this.ascending;
-      } else {
-        this.ascending = true;
-        this.sortColumn = col;
-      }
 
-      var ascending = this.ascending;
-
-      this.items.sort(function (a, b) {
-        if (a[col] > b[col]) {
-          return ascending ? 1 : -1;
-        } else if (a[col] < b[col]) {
-          return ascending ? -1 : 1;
-        }
-        return 0;
-      });
-    },
-  },
   computed: {
     columns: function columns() {
       if (this.items.length == 0) {
