b108b2a040627e771c45d2cb49f31cbc10f4a832
Asef <50338626+Asef00@users.noreply.github.com>
2022-06-04 12:28:45 -0400

    data structure: items prop

--- components/VTable.vue
+++ components/VTable.vue
@@ -72,7 +72,11 @@
         </tr>
       </thead>
       <tbody class="c-table__body">
+        <tr v-if="!items">
+          <td colspan="100%" class="u-text-center">No Data</td>
+        </tr>
         <tr
+          v-else
           :class="[item['note'] > 0 ? 'has-note' : '', 'c-table__row']"
           v-for="item in items"
           :key="item.id"
@@ -149,84 +153,13 @@ export default {
       type: Array,
       required: true,
     },
+    items: {
+      type: Array,
+    },
   },
   data() {
     return {
       sortColumn: "",
-      items: [
-        {
-          id: 1,
-          full_name: "Chandler Bing",
-          source: "Realtyna.com",
-          MLS: ["Treb", "MFR MLS", "XMLS"],
-          paid_so_far: "$8,400",
-          email: "Chandler@gmail.com",
-          status: "Inactive",
-          note: null,
-          _verified: true,
-          _increment: false,
-        },
-        {
-          id: 2,
-          full_name: "Ross Geller",
-          source: "Realtyna.com",
-          MLS: ["MFR MLS"],
-          paid_so_far: "$3,200",
-          email: "Ross@gmail.com",
-          status: "active",
-          note: null,
-          _verified: false,
-          _increment: true,
-        },
-        {
-          id: 3,
-          full_name: "Rachel Green",
-          source: "Houzes",
-          MLS: ["Treb", "MFR MLS"],
-          paid_so_far: "$1,400",
-          email: "Rachel@gmail.com",
-          status: "active",
-          note: null,
-          _verified: false,
-          _increment: true,
-        },
-        {
-          id: 4,
-          full_name: "Monica Geller",
-          source: "Realtyna.com",
-          MLS: ["XMLS"],
-          paid_so_far: "$12,400",
-          email: "Monica@gmail.com",
-          status: "Inactive",
-          note: 2,
-          _verified: true,
-          _increment: false,
-        },
-        {
-          id: 5,
-          full_name: "Joey Tribbiani",
-          source: "Realtyna.com",
-          MLS: ["ITech MLS"],
-          paid_so_far: "$9,450",
-          email: "Joey@gmail.com",
-          status: "active",
-          note: null,
-          _verified: true,
-          _increment: false,
-        },
-        {
-          id: 6,
-          full_name: "Phoebe Buffay",
-          source: "Realtyna.com",
-          MLS: [],
-          paid_so_far: "$6,100",
-          email: "Phoebe@gmail.com",
-          status: "Inactive",
-          note: null,
-          _verified: false,
-          _increment: true,
-        },
-      ],
     };
   },
 };
