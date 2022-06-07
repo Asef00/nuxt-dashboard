55723d7da024a483a0bf014955a0a616c2fb3745
Alan <alan.b@realtyna.net>
2022-06-07 12:14:43 -0400

    install package json editor

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/mixin"
+    "~/plugins/mixin",
+    { src: '~/plugins/jsonEditor', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
