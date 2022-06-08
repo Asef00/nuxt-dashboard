48a05f783d155eef7a660327958aec05ea597992
Asef <50338626+Asef00@users.noreply.github.com>
2022-06-08 15:26:59 -0400

    card component toolbar

---
+++ pages/mls.vue
@@ -0,0 +1,60 @@
+<template>
+  <VCard title="MLS Information">
+    <template #header>
+      <VBtn customClass="m-0">Button</VBtn>
+    </template>
+
+    <form action="" class="c-form">
+      <h4 class="c-form__title">Sync Accounts</h4>
+    </form>
+  </VCard>
+</template>
+
+<script>
+import * as Yup from "yup";
+
+export default {
+  data() {
+    return {
+      payload: {
+        name: "",
+        tag: "",
+        summary: "",
+      },
+      options: ["list", "of", "options", "bla"],
+      toggleActive: false,
+      selected: "", //for checkbox
+      selectedOptions: [], //for checkbox
+    };
+  },
+
+  methods: {
+    validation() {
+      return Yup.object({
+        name: Yup.string().required(),
+        summary: Yup.string().required(),
+        tag: Yup.array().min(1),
+      });
+    },
+
+    resetError() {
+      this.$store.commit("me/RESET_ERROR");
+      this.errors = {
+        name: "",
+        tag: "",
+        summary: "",
+      };
+    },
+
+    triggerToggleEvent(value) {
+      this.toggleActive = value;
+    },
+  },
+
+  created() {
+    this.setTitle("Treb MLS");
+    this.setBreadcrumb([]);
+    this.resetError();
+  },
+};
+</script>
