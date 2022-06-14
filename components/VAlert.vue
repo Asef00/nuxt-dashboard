7fe74111fdbdc3cceaff21efd4e6c0779409c78c
Peter <peter.h@realtyna.net>
2022-06-14 16:49:32 -0400

    add animation + close btn

--- components/VAlert.vue
+++ components/VAlert.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="c-alert" role="alert">
-    <slot/>
-  </div>
+  <transition>
+    <div v-if="showAlert" class="c-alert" role="alert">
+      <slot />
+      <button class="c-alert__close" @click="showAlert = false"></button>
+    </div>
+  </transition>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showAlert: true,
+    };
+  },
+};
 </script>
