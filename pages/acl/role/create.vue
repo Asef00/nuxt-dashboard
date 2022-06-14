e821b1af3b4ff093e6d14efeb6c4e442b240bc17
Alan <alan.b@realtyna.com>
2022-06-14 07:54:23 -0400

    fix get list data in role and change store

--- pages/acl/role/create.vue
+++ pages/acl/role/create.vue
@@ -109,7 +109,7 @@ export default {
       }
     },
     async getPermission() {
-      await this.$store.dispatch('permission/list')
+      await this.$store.dispatch('permission/list',{})
       let err = this.handleError(this.$store.state.permission.error)
       if (!err) {
         this.list.permission = this.$store.state.permission.list
