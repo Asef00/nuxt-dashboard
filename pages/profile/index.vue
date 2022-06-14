bb62c16315cc8218c393543ca2460e7c17182539
Peter <peter.h@realtyna.net>
2022-06-14 18:08:40 -0400

    add verified + add label slot for input + edit btn

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -2,6 +2,14 @@
   <div>
     <VCard title="Profile">
       <template #header>
+        <VBtn
+          @action="editMode = !editMode"
+          btn="info"
+          type="button"
+          class="m-0 c-btn--small"
+        >
+          Edit Profile
+        </VBtn>
         <VBtn @action="showModal = true" type="button" class="m-0 c-btn--small">
           Change Password
         </VBtn>
@@ -14,111 +22,49 @@
         </div>
         <div class="col-md-8 col-12">
           <form action="" class="c-form">
-            <div class="row mb-3">
-              <div class="col">
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Full Name</label>
-                  <span class="u-text-secondary">
-                    Toronto Regional Real Estate Board
-                  </span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Feed Type</label>
-                  <span class="c-badge u-bg-light u-text-secondary">IDX</span>
-                </div>
+            <h4 class="c-form__title">Profile Info</h4>
+            <div class="row">
+              <div class="col-md-6">
+                <VInput
+                  @validation="validate('firstname')"
+                  :error="errorMessage('firstname')"
+                  :disabled="!editMode"
+                  label="First Name"
+                  v-model="payload.firstname"
+                  placeholder="Enter your first name"
+                />
               </div>
-
-              <div class="col">
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Abbreviated</label>
-                  <span class="u-text-secondary">Treb MLS</span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Credentials</label>
-                  <NuxtLink to="/" class="u-text-info">Click to View</NuxtLink>
-                  <span class="c-badge u-bg-danger">Revoke</span>
-                </div>
+              <div class="col-md-6">
+                <VInput
+                  @validation="validate('lastname')"
+                  :error="errorMessage('lastname')"
+                  :disabled="!editMode"
+                  label="Last Name"
+                  v-model="payload.lastname"
+                  placeholder="Enter your last name"
+                />
               </div>
-            </div>
-
-            <h4 class="c-form__title mb-2">Details</h4>
-            <div class="row mb-3">
-              <div class="col">
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Address</label>
-                  <span class="u-text-secondary">
-                    456 Prentiss St, San Francisco, USA
-                  </span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Coverage Area</label>
-                  <span class="u-text-secondary"
-                    >4617, 7819, 1012, Florida</span
-                  >
-                </div>
-              </div>
-
-              <div class="col">
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label"># of Clients</label>
-                  <span class="u-text-secondary">873</span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label"># of Agents</label>
-                  <span class="u-text-secondary">100</span>
-                </div>
+              <div class="col-md-6">
+                <VInput
+                  @validation="validate('email')"
+                  :error="errorMessage('email')"
+                  :disabled="!editMode"
+                  label="Email"
+                  v-model="payload.email"
+                  placeholder="Enter your Email"
+                >
+                  <template #label>
+                    <span
+                      v-if="payload.is_verified"
+                      class="c-badge u-bg-success"
+                    >
+                      Verified
+                    </span>
+                    <span v-else class="c-badge u-bg-danger">Not Verified</span>
+                  </template>
+                </VInput>
               </div>
             </div>
-
-            <span class="c-form__divider"></span>
-
-            <h4 class="c-form__title mb-2">Contact Information For IDX</h4>
-            <div class="row mb-3">
-              <div class="col">
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Website</label>
-                  <span class="u-text-link">sitename.com/mls</span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Contact 1</label>
-                  <span class="u-text-secondary">00 55 369 888 3365</span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Contact 2</label>
-                  <span class="u-text-secondary">00 55 369 888 3366</span>
-                </div>
-              </div>
-
-              <div class="col">
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Contact Num</label>
-                  <span class="u-text-secondary">00 225 558 9966</span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Contact Ticket</label>
-                  <span class="u-text-secondary">RFK-662</span>
-                </div>
-
-                <div class="c-form__control c-form__control--inline mb-0">
-                  <label class="c-form__label">Contact Email</label>
-                  <span class="u-text-secondary">mls@trebmls.com</span>
-                </div>
-              </div>
-            </div>
-
-            <span class="c-badge u-bg-danger">danger</span>
-            <span class="c-badge u-bg-warn">warn</span>
-            <span class="c-badge u-bg-info">info</span>
-            <span class="c-badge u-bg-success">success</span>
-            <span class="c-badge">badge</span>
-            <span class="c-badge u-bg-dark">success</span>
           </form>
         </div>
       </div>
@@ -167,13 +113,20 @@
 </template>
 
 <script>
+import * as Yup from "yup";
+
 export default {
   data() {
     return {
       showModal: false,
+      editMode: false,
 
       payload: {
-        name: "",
+        password: "",
+        firstname: "John",
+        lastname: "Doe",
+        email: "johndoe@gmail.com",
+        is_verified: true,
       },
     };
   },
@@ -182,6 +135,8 @@ export default {
     validation() {
       return Yup.object({
         password: Yup.string().required(),
+        firstname: Yup.string().required(),
+        lastname: Yup.string().required(),
       });
     },
   },
