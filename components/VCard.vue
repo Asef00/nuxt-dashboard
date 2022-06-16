bee6671ed4e3f9283ac509f88d6a0ac8ef36946c
Peter <peter.h@realtyna.net>
2022-06-16 18:23:01 -0400

    progress-bar component

--- components/VCard.vue
+++ components/VCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="c-card">
-    <div class="c-card__header">
+    <div class="c-card__header" v-if="title">
       <h2 class="c-card__title">{{ title }}</h2>
       <div class="c-card__toolbar" v-if="this.$slots.header">
         <slot name="header"/>
---
+++ pages/dashboard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="o-page c-page--dashboard">
+    <VCard>
+      <div class="c-report">
+        <div class="c-report__title">Alerts</div>
+        <span class="c-report__big">536</span>
+        <span class="c-report__change u-text-active">(+25%)</span>
+        <span class="c-report__icon">
+          <fa icon="triangle-exclamation" size="lg" />
+        </span>
+      </div>
+      <VProgress
+        bar="primary"
+        title="overview of last month"
+        subtitle="Monthly"
+        :value="alertValue"
+      />
+    </VCard>
+    <VCard>
+      <div class="c-report">
+        <div class="c-report__title">MLS Clients Active Requests</div>
+        <span class="c-report__big">128</span>
+        <span class="c-report__change u-text-active">(+25%)</span>
+        <span class="c-report__icon">
+          <fa icon="file-lines" size="lg" />
+        </span>
+      </div>
+      <VProgress
+        bar="info"
+        title="overview of last month"
+        subtitle="Monthly"
+        :value="activeRequests0Value"
+      />
+    </VCard>
+    <VCard>
+      <div class="c-report">
+        <div class="c-report__title">MLS Clients</div>
+        <span class="c-report__big">1.2K</span>
+        <span class="c-report__change u-text-inactive">(-11%)</span>
+        <span class="c-report__icon">
+          <fa icon="users" size="lg" />
+        </span>
+      </div>
+      <VProgress
+        bar="success"
+        title="overview of last month"
+        subtitle="Monthly"
+        :value="clientsValue"
+      />
+    </VCard>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      alertValue: 40,
+      activeRequests0Value: 60,
+      clientsValue: 30,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.c-report {
+  font-weight: 600;
+}
+.c-report__icon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: #e2e1ea;
+  margin: 1.25em 1.56em;  
+}
+.c-report__big {
+  @include font-size(32px);
+  color: #182d49;
+}
+.c-report__change {
+  @include font-size(14px);
+}
+</style>
