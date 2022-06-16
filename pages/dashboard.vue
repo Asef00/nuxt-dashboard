39c5bb8b9a0bc35725b821069011ea2e831c8a01
Peter <peter.h@realtyna.net>
2022-06-16 18:43:30 -0400

    add sample table

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -16,6 +16,7 @@
         :value="alertValue"
       />
     </VCard>
+
     <VCard>
       <div class="c-report">
         <div class="c-report__title">MLS Clients Active Requests</div>
@@ -32,6 +33,7 @@
         :value="activeRequests0Value"
       />
     </VCard>
+
     <VCard>
       <div class="c-report">
         <div class="c-report__title">MLS Clients</div>
@@ -48,17 +50,85 @@
         :value="clientsValue"
       />
     </VCard>
+
+    <VCard :loader="loaderRequest">
+      <VTable
+        @changePage="changePage($event)"
+        @changePerPage="changePerPage($event)"
+        :table="table"
+      />
+    </VCard>
   </div>
 </template>
 
 <script>
 export default {
   data() {
+    let _this = this;
     return {
       alertValue: 40,
       activeRequests0Value: 60,
       clientsValue: 30,
+      table: {
+        columns: [
+          { key: "id", label: "#" },
+          { key: "full_name", label: "Full Name" },
+          { key: "username", label: "Username" },
+          { key: "status", label: "Status" },
+          { key: "created_at", label: "Created At", class: "u-text-center" },
+          { key: "updated_at", label: "Updated At", class: "u-text-center" },
+          {
+            key: "action",
+            label: '<img src="/img/gear.svg" alt="" />',
+            class: "u-text-center",
+          },
+        ],
+        items: [],
+        map: {
+          action(item) {
+            return `<NuxtLink v-if="can('person.update')" to="/person/edit/${item.id}" class="c-badge u-bg-info">Edit</NuxtLink> |
+                <NuxtLink v-if="can('person.show')" to="/person/${item.id}" class="c-badge u-bg-primary">Details</NuxtLink>`;
+          },
+          created_at(item) {
+            return _this.dateFormat(item.created_at);
+          },
+          updated_at(item) {
+            return _this.dateFormat(item.updated_at);
+          },
+          full_name(item) {
+            return `${item.name} ${item.family_name}`;
+          },
+          status(item) {
+            return item.enabled
+              ? `<span class="c-badge u-bg-success">Enable</span>`
+              : `<span class="c-badge u-bg-danger">Disable</span>`;
+          },
+          //REQUIRED
+          rowClass() {},
+        },
+      },
     };
+  },
+
+  methods: {
+    async list(page = null, limit = null) {
+      this.startLoading();
+      this.$store.commit("person/RESET_ERROR");
+      await this.$store.dispatch("person/list", {
+        page: page ?? this.getPaginate(),
+        limit: limit ?? this.getLimit(),
+        paginate: 1,
+      });
+      let err = this.handleError(this.$store.state.person.error);
+      if (!err) {
+        this.table.items = this.$store.state.person.list;
+      }
+      this.stopLoading();
+    },
+  },
+  
+  created() {
+    this.list();
   },
 };
 </script>
