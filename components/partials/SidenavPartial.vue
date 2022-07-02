2f12fee59892ffb9a681f77c2542feb74c4b7589
Peter <peter.h@realtyna.net>
2022-07-02 10:55:51 -0400

    sidenav scrollable + custom scrollbar

--- components/partials/SidenavPartial.vue
+++ components/partials/SidenavPartial.vue
@@ -1,110 +1,121 @@
 <template>
   <div class="c-sidenav js-sidenav">
-    <ul class="c-sidenav__list" role="presentation">
-      <li class="c-sidenav__header">
-        <NuxtLink to="/" class="c-logo">
-          <VIcon icon="logo"/>
-        </NuxtLink>
-        <span class="c-sidenav__close js-menu-close">
-          <fa icon="xmark"/>
-        </span>
-      </li>
-      <li class="c-sidenav__item pr-3 pl-3">
-        <input type="text" class="c-sidenav__search" placeholder="Search ..."/>
-      </li>
-      <li class="c-sidenav__item">
-        <NuxtLink to="/" class="c-sidenav__link">
-          <VIcon icon="dashboard"/>
-          <span>Dashboard</span>
-        </NuxtLink>
-      </li>
-      <li v-if="can('person.index')" class="c-sidenav__item">
-        <NuxtLink to="/person" class="c-sidenav__link">
-          <VIcon icon="users"/>
-          <span>Person</span>
-        </NuxtLink>
-      </li>
-      <li v-if="can('person.product.index')" class="c-sidenav__item">
-        <NuxtLink to="/person-product" class="c-sidenav__link">
-          <VIcon icon="products"/>
-          <span>Products</span>
-        </NuxtLink>
-      </li>
-      <VDropdown
-        v-if="
-          can([
-            'role.index',
-            'permission.index',
-            'field-type.index',
-            'field-name.index',
-            'model.index',
-            'license-mode.index',
-            'product.index',
-          ])
-        "
-        class="c-sidenav__item"
-        btnClass="c-sidenav__link"
-        position="right"
-        wrapper="li"
-      >
-        <template #btn>
-          <VIcon width="24" height="23" icon="note-1"/>
-          <span>Definitions</span>
-        </template>
-        <template #menu>
-          <NuxtLink
-            v-if="can('product.index')"
-            to="/product"
-            class="c-dropdown__item"
-          >Products
+    <vue-custom-scrollbar>
+      <ul class="c-sidenav__list" role="presentation">
+        <li class="c-sidenav__header">
+          <NuxtLink to="/" class="c-logo">
+            <VIcon icon="logo" width="80" height="60" />
           </NuxtLink>
-          <NuxtLink
-            v-if="can('license-mode.index')"
-            to="/license-mode"
-            class="c-dropdown__item"
-          >License modes
+          <span class="c-sidenav__close js-menu-close">
+            <fa icon="xmark" />
+          </span>
+        </li>
+        <li class="c-sidenav__item pr-3 pl-3">
+          <input
+            type="text"
+            class="c-sidenav__search"
+            placeholder="Search ..."
+          />
+        </li>
+        <li class="c-sidenav__item">
+          <NuxtLink to="/" class="c-sidenav__link">
+            <VIcon icon="dashboard" />
+            <span>Dashboard</span>
           </NuxtLink>
-          <NuxtLink
-            v-if="can('group.index')"
-            to="/group"
-            class="c-dropdown__item"
-          >Groups
+        </li>
+        <li v-if="can('person.index')" class="c-sidenav__item">
+          <NuxtLink to="/person" class="c-sidenav__link">
+            <VIcon icon="users" />
+            <span>Person</span>
           </NuxtLink>
-          <NuxtLink
-            v-if="can('role.index')"
-            to="/acl/role"
-            class="c-dropdown__item"
-          >Roles
+        </li>
+        <li v-if="can('person.product.index')" class="c-sidenav__item">
+          <NuxtLink to="/person-product" class="c-sidenav__link">
+            <VIcon icon="products" />
+            <span>Products</span>
           </NuxtLink>
-          <NuxtLink
-            v-if="can('permission.index')"
-            to="/acl/permission"
-            class="c-dropdown__item"
-          >Permissions
-          </NuxtLink>
-          <NuxtLink
-            v-if="can('field-type.index')"
-            to="/field/type"
-            class="c-dropdown__item"
-          >Field types
-          </NuxtLink>
-          <NuxtLink
-            v-if="can('field-name.index')"
-            to="/field/name"
-            class="c-dropdown__item"
-          >Field names
-          </NuxtLink>
-        </template>
-      </VDropdown>
-    </ul>
+        </li>
+        <VDropdown
+          v-if="
+            can([
+              'role.index',
+              'permission.index',
+              'field-type.index',
+              'field-name.index',
+              'model.index',
+              'license-mode.index',
+              'product.index',
+            ])
+          "
+          class="c-sidenav__item"
+          btnClass="c-sidenav__link"
+          position="right"
+          wrapper="li"
+        >
+          <template #btn>
+            <VIcon width="24" height="23" icon="note-1" />
+            <span>Definitions</span>
+          </template>
+          <template #menu>
+            <NuxtLink
+              v-if="can('product.index')"
+              to="/product"
+              class="c-dropdown__item"
+              >Products
+            </NuxtLink>
+            <NuxtLink
+              v-if="can('license-mode.index')"
+              to="/license-mode"
+              class="c-dropdown__item"
+              >License modes
+            </NuxtLink>
+            <NuxtLink
+              v-if="can('group.index')"
+              to="/group"
+              class="c-dropdown__item"
+              >Groups
+            </NuxtLink>
+            <NuxtLink
+              v-if="can('role.index')"
+              to="/acl/role"
+              class="c-dropdown__item"
+              >Roles
+            </NuxtLink>
+            <NuxtLink
+              v-if="can('permission.index')"
+              to="/acl/permission"
+              class="c-dropdown__item"
+              >Permissions
+            </NuxtLink>
+            <NuxtLink
+              v-if="can('field-type.index')"
+              to="/field/type"
+              class="c-dropdown__item"
+              >Field types
+            </NuxtLink>
+            <NuxtLink
+              v-if="can('field-name.index')"
+              to="/field/name"
+              class="c-dropdown__item"
+              >Field names
+            </NuxtLink>
+          </template>
+        </VDropdown>
+      </ul>
+    </vue-custom-scrollbar>
   </div>
 </template>
 
 <script>
+import vueCustomScrollbar from "vue-custom-scrollbar";
+import "vue-custom-scrollbar/dist/vueScrollbar.css";
+
 export default {
   name: "SidenavPartial",
-  components: {},
 
+  components: {
+    vueCustomScrollbar,
+  },
 
   watch: {
     $route: {
