b930c0fb77f9f1eae527b054fc83b0f8e62e64e9
Peter <peter.h@realtyna.net>
2022-07-16 11:26:23 -0400

    refactor change password modal

--- components/page/person/ChangePassword.vue
+++ components/page/person/ChangePassword.vue
@@ -1,9 +1,5 @@
 <template>
-  <VModal
-    :showModal="show"
-    @close="show = false"
-    title="Change password"
-  >
+  <VModal :showModal="show" @close="hide()" title="Change password">
     <form @submit.prevent="changePassword" class="c-form">
       <div class="row">
         <div class="col-md-12">
@@ -16,17 +12,22 @@
             type="password"
           />
           <VSwitch
+            class="mb-0"
             label="Force Password Change"
             v-model="payload.force_change_password"
             :checked="payload.force_change_password"
           />
         </div>
       </div>
-      <div class="mt-1">
-        <VBtn :loader="loaderRequest">Change Password</VBtn>
-        <VBtn btn="danger" @action="show = false" :loader="loaderRequest">CANCEL</VBtn>
-      </div>
     </form>
+    <template #footer>
+      <VBtn class="mb-0" @action="changePassword()" :loader="loaderRequest">
+        Change Password
+      </VBtn>
+      <VBtn class="mb-0" btn="danger" @action="hide()" :loader="loaderRequest">
+        CANCEL
+      </VBtn>
+    </template>
   </VModal>
 </template>
 
@@ -35,29 +36,37 @@ import * as Yup from "yup";
 
 export default {
   name: "ChangePassword",
+
   props: {
     id: {
       type: [String, Number],
-      required: true
+      required: true,
     },
+
     show: {
       type: Boolean,
-      required: false
-    }
+      required: false,
+    },
   },
+
   data() {
     return {
       payload: {
         force_change_password: true,
-        password: '',
-      }
+        password: "",
+      },
     };
   },
+
   methods: {
+    hide() {
+      this.show = false;
+    },
+
     changePassword() {
       this.startLoading();
       this.validation()
-        .validate(this.payload, {abortEarly: false})
+        .validate(this.payload, { abortEarly: false })
         .then(async () => {
           this.resetError();
           await this.$store.dispatch("person/changePassword", {
@@ -65,12 +74,12 @@ export default {
             payload: {
               permanent: !this.payload.force_change_password,
               password: this.payload.password,
-            }
+            },
           });
           this.stopLoading();
           const err = this.handleError(this.$store.state.person.error);
           if (!err) {
-            this.show = false;
+            this.hide();
             this.$toast.success("Password successfully change.");
           }
         })
@@ -79,6 +88,7 @@ export default {
           this.stopLoading();
         });
     },
+
     validation() {
       let roles = {
         password: Yup.string().required(),
@@ -86,25 +96,27 @@ export default {
       };
       return Yup.object(roles);
     },
+
     resetError() {
-      this.$store.commit('person/RESET_ERROR')
+      this.$store.commit("person/RESET_ERROR");
       this.errors = {
-        force_change_password: '',
-        password: '',
+        force_change_password: "",
+        password: "",
       };
     },
   },
+
   created() {
-    this.resetError()
+    this.resetError();
   },
+
   watch: {
     show(val) {
-      this.$emit('show', val);
-    }
-  }
-}
+      this.$emit("show", val);
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
