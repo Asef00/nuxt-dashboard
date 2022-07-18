ef25504e7b2a1e9ea25a92eedd5a5fd687e61af9
Peter <peter.h@realtyna.net>
2022-07-18 12:13:08 -0400

    Avatar component

---
+++ components/VAvatar.vue
@@ -0,0 +1,36 @@
+<template>
+  <Avatar
+    backgroundColor="#fff"
+    :username="username"
+    :size="Number(size)"
+    :customStyle="{ 'font-weight': 'bold' }"
+  />
+</template>
+
+<script>
+import Avatar from "vue-avatar";
+
+export default {
+  name: "VAvatar",
+
+  components: {
+    Avatar,
+  },
+
+  props: {
+    size: {
+      type: String,
+      default: "",
+    },
+  },
+
+  computed: {
+    username() {
+      return `${this.$auth.user.name} ${this.$auth.user.family_name}`;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
